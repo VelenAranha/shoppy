f174c1505e39a7a48b00f6239cb8fe4b40b4ed35
Velen Aranha <t-velen.aranha@paypay-corp.co.jp>
2022-01-27 18:23:02 +0530

    Add headder component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
